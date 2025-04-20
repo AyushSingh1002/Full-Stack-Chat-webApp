42919730c015b81002c797428904a98b9e7cc55b
AYUSH KUMAR SINGH <ayushsinghzzzzz38@gmail.com>
2025-04-20 17:06:21 +0530

    Update token.js

--- backend/src/lib/token.js
+++ backend/src/lib/token.js
@@ -1,15 +1,18 @@
-
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 const generateTokens = (userId, res) => {
-  const token = jwt.sign({userId},process.env.JWT_SECRET, {
-    expiresIn : "7d"
-  })
- res.cookie("token",token,{
-    httpOnly: true,
-    sameSite : "strict",
- })
- return token
-}
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
 
-export {generateTokens}
+  res.cookie("token", token, {
+    httpOnly: true,
+    sameSite: "strict",
+    secure: false, // true if using HTTPS
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
+
+  return token;
+};
+
+export { generateTokens };
